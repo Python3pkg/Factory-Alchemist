8b728589a525ae5691e67514e1a6438f60e1063e
Eduardo Matos <eduardo.matos@sieve.com.br>
2015-10-23 08:14:15 -0200

    bare bones

---
+++ runtests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import pytest
+
+
+os.environ['test'] = '1'
+
+args = ['-rsxX', '--tb=native', '--cov', 'sqlalchemy_mommy', '--cov-config', '.coveragerc',
+                '--cov-report', 'html', '--cov-report', 'term-missing'] + sys.argv[1:]
+
+sys.exit(pytest.main(args + ['tests/']))
+
